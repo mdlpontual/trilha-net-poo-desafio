6d88fa00b2afa18f1e31abf0bf41f604baffe670
mdlpontual <mdlpontual@gmail.com>
2025-09-10 18:56:48 -0300

    refactor: implementar requisitos da atividade de copmclusão do módulo - POO

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        // Modificação mdlpontual: adicionar construtor
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria) { }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.Write($"O smartphone {Modelo} está instalando o aplicativo {nomeApp}!" + "\n");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphonee
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokiaTijolao = new Nokia("12345", "3310", "0101010101", 4);
+nokiaTijolao.Ligar();
+nokiaTijolao.ReceberLigacao();
+nokiaTijolao.InstalarAplicativo("MSN Messenger");
+
+
+Smartphone iphoneMaxPro = new Iphone("67890", "17 Max Pro", "1010101010", 256);
+nokiaTijolao.Ligar();
+nokiaTijolao.ReceberLigacao();
+nokiaTijolao.InstalarAplicativo("iTunes");
